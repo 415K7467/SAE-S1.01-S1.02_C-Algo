ae18318939458e6aa3bd7347cda4394bd6cbbaab
Vincent Chavot--Dambrun <vincent.chavot----dambrun@etu.univ-lyon1.fr>
2022-01-06 08:45:57 +0100

    separation de recherche et trie

--- fonctions.h
+++ fonctions.h
@@ -1,14 +1,25 @@
 #ifndef SAE_S1_01_S1_02_C_ALGO_FONCTIONS_H
 #define SAE_S1_01_S1_02_C_ALGO_FONCTIONS_H
 
+//main//
+void structure();
+int menu(FILE *fic, char tuple[]);
+int main();
+
+//ajouter-modifier-supprimmer//
 int ajouter(FILE *fic);
-char afficher_tout(FILE *fic, char tuple[]);
-int compter(FILE *fic, int nb_personne, char tuple[]);
+
+//trie//
 int trier(FILE *fic, int trie);
 void switch_tri(FILE *fic, int trie,int x);
 int menu_tri(FILE *fic, int trie, char tuple[]);
+
+//recherche//
 int menu_recherche(FILE *fic, int trie, char tuple[]);
-int menu(FILE *fic, char tuple[]);
+
+//other//
+char afficher_tout(FILE *fic, char tuple[]);
+int compter(FILE *fic, int nb_personne, char tuple[]);
 
 
 #endif //SAE_S1_01_S1_02_C_ALGO_FONCTIONS_H
--- tri_recherche.c
+++ tri_recherche.c
@@ -6,72 +6,7 @@
 //#define chemin "test.csv"
 #define chemin "C:/Users/Administrateur/OneDrive - etu.univ-lyon1.fr/Documents/S1/SAE/SAE1.2_C-algo/SAE-S1.01-S1.02_C-Algo/test.csv"
 
-//----------------------------------------------Trie----------------------------------------------//
-int trier(FILE *fic, int trie){
-    trie=1;
-}
-
-void switch_tri(FILE *fic, int trie,int x){
-    switch(x) {
-        case 1 :
-            if (trie != 1) {
-                trier(fic, 1);
-            }
-            break;
-        case 2 :
-            if (trie != 2) {
-                trier(fic, 2);
-            }
-            break;
-        case 3 :
-            if (trie != 3) {
-                trier(fic, 3);
-            }
-            break;
-        case 4 :
-            if (trie != 4) {
-                trier(fic, 4);
-            }
-            break;
-        case 5 :
-            if (trie != 5) {
-                trier(fic, 5);
-            }
-            break;
-        case 6 :
-            if (trie != 6) {
-                trier(fic, 6);
-            }
-            break;
-        case 7 :
-            if (trie != 7) {
-                trier(fic, 7);
-            }
-            break;
-    }
-}
-
 //----------------------------------------------Recherche----------------------------------------------//
-
-
-//----------------------------------------------Menu----------------------------------------------//
-
-int menu_tri(FILE *fic, int trie, char tuple[]){
-    int nb_personnes=0,x;
-    printf("choisir ce que l'on recherche:");
-    printf("\n\t1-tri par prénom");
-    printf("\n\t2-tri par nom");
-    printf("\n\t3-tri par ville");
-    printf("\n\t4-tri par code postal");
-    printf("\n\t5-tri par numéro de téléphone");
-    printf("\n\t6-tri par mail");
-    printf("\n\t7-tri par métier\n");
-    scanf("%d", &x);
-    if (x<=7){
-        switch_tri(fic,trie,x);
-    }
-}
-
 int menu_recherche(FILE *fic, int trie, char tuple[]){
     int nb_personnes=0,x;
     printf("choisir ce que l'on recherche:");
